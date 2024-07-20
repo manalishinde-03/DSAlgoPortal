ab68564e64fac0219e34f7de60fee36ff9e5e14c
Manali Shinde <manalishinde03@gmail.com>
2024-07-20 16:17:12 -0500

    Updates in runner file

--- DSAlgoPortal/src/test/java/runner/ParallelTestRunner.java
+++ DSAlgoPortal/src/test/java/runner/ParallelTestRunner.java
@@ -12,7 +12,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
 		plugin = {"pretty","html:target/report/parallel.html","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
 		//tags= "@TC_06",
-		features = {"src/test/resources/features"}, //location of feature files
+		features = {"src/test/resources/features/Home.feature","src/test/resources/features/Login.feature","src/test/resources/features/Register.feature"}, //location of feature files
 		glue= "stepDefinitions" //location of step definition files
 		) 
 
